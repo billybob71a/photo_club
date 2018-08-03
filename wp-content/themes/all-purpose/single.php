509a4f7a0f05f74103534ff1aac937c0ad27ad9d
Peter Yung <yungpet@gmail.com>
2018-08-02 23:25:02 -0600

    modify to create clickable link for deleting a post

--- wp-content/themes/all-purpose/single.php
+++ wp-content/themes/all-purpose/single.php
@@ -19,7 +19,8 @@ get_header(); ?>
 				
 				<div class="postpagination">
 					<!--Commented out by PeterY <?php the_post_navigation(); ?> -->
-					<?php if (function_exists('get_delete_post_link')) {printf('Delete this post');} ?>
+					<?php if (function_exists('get_delete_post_link')) {
+					    echo ('<p><a href="<?php echo get_delete_post_link( $post->ID ) ?>">Delete post</a></p>');} ?>
 				</div>
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template.
